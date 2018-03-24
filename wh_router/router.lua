8d69906de1e367b47600c1dca4cd8b53bc1b803a
Roman Petrov <megus.sugem@gmail.com>
2018-03-24 12:37:27 +0300

    Some new changes

--- wh_router/router.lua
+++ wh_router/router.lua
@@ -9,8 +9,8 @@ local M = {}
 
 --- Router message hashes
 M.messages = {
-    scene_input = hash("wh_router_scene_input"),      -- scene input message
-    scene_popped = hash("wh_router_scene_popped")     -- scene popped message
+	scene_input = hash("wh_router_scene_input"),      -- scene input message
+	scene_popped = hash("wh_router_scene_popped")     -- scene popped message
 }
 
 ----------------------------------------------------------------------------------------------------
@@ -19,19 +19,19 @@ M.messages = {
 
 -- Router messages
 local messages = {
-    push = hash("wh_router_scene_push"),
-    push_modal = hash("wh_router_scene_push_modal"),
-    popup = hash("wh_router_scene_popup"),
-    close = hash("wh_router_scene_close")
+	push = hash("wh_router_scene_push"),
+	push_modal = hash("wh_router_scene_push_modal"),
+	popup = hash("wh_router_scene_popup"),
+	close = hash("wh_router_scene_close")
 }
 
 -- Scene display methods
 local methods = {
-    switch = 0,
-    push = 1,
-    push_modal = 2,
-    popup = 3,
-    restore = 4
+	switch = 0,
+	push = 1,
+	push_modal = 2,
+	popup = 3,
+	restore = 4
 }
 
 -- Private storage: routing tables and router objects
@@ -40,51 +40,130 @@ local routing_tables = {}
 
 -- Returns scene controller URL string
 local function scene_controller_url(ro, name)
-    return name .. ":/" .. ro.scene_controller_path
+	return name .. ":/" .. ro.scene_controller_path
 end
 
 -- Unload a scene
 local function unload_scene(url)
-    msg.post(url, "disable")
-    msg.post(url, "final")
-    msg.post(url, "unload")
+	msg.post(url, "disable")
+	msg.post(url, "final")
+	msg.post(url, "unload")
 end
 
 -- Puts the next scene to the scene stack and loads it
 local function next_scene(ro, method, message)
-    local scene
-    local sync = true
+	local scene
+	local sync = true
 
-    if method == methods.switch then
-        -- Switch a scene, use the state machine to get the next scene
-        local current_name = "first_scene"
-        if #ro.stack ~= 0 then
-            current_name = ro.stack[#ro.stack].name
-            -- Save the state of the current scene
-            ro.states[current_name] = message.state
-        end
-        local next_name, input = routing_tables[ro.router_url][current_name](message and message.output or nil)
-        scene = {name = next_name, input = input, method = method}
-    elseif method == methods.push or method == methods.push_modal or method == methods.popup then
-        -- All these methods are pushing the new scene
-        if method == methods.push then
-            ro.states[ro.stack[#ro.stack].name] = message.state
-        end
-        scene = {name = message.scene_name, input = message.input, method = method}
-        sync = message.sync
-    elseif method == methods.restore then
-        -- Restore the previous scene
-        local previous = ro.stack[#ro.stack - 1]
-        scene = {name = previous.name, input = previous.input,
-            output = message.output, method = previous.method, restored = true}
-        -- Remove it for now, it will be pushed back below
-        table.remove(ro.stack, #ro.stack - 1)
-    end
-    -- Push the next scene to the stack
-    table.insert(ro.stack, scene)
-    msg.post("#" .. scene.name, sync and "load" or "async_load")
+	if method == methods.switch then
+		-- Switch a scene, use the state machine to get the next scene
+		local current_name = "first_scene"
+		if #ro.stack ~= 0 then
+			current_name = ro.stack[#ro.stack].name
+			-- Save the state of the current scene
+			ro.states[current_name] = message.state
+		end
+		local next_name, input = routing_tables[ro.router_url][current_name](message and message.output or nil)
+		scene = {name = next_name, input = input, method = method}
+	elseif method == methods.push or method == methods.push_modal or method == methods.popup then
+		-- All these methods are pushing the new scene
+		if method == methods.push then
+			ro.states[ro.stack[#ro.stack].name] = message.state
+		end
+		scene = {name = message.scene_name, input = message.input, method = method}
+		sync = message.sync
+	elseif method == methods.restore then
+		-- Restore the previous scene
+		local previous = ro.stack[#ro.stack - 1]
+		scene = {name = previous.name, input = previous.input,
+			output = message.output, method = previous.method, restored = true}
+		-- Remove it for now, it will be pushed back below
+		table.remove(ro.stack, #ro.stack - 1)
+	end
+	-- Push the next scene to the stack
+	table.insert(ro.stack, scene)
+	msg.post("#" .. scene.name, sync and "load" or "async_load")
 end
 
+
+-- Message handling
+local message_handlers = {
+	-- A new scene is loaded
+	[hash("proxy_loaded")] = function(ro, _, sender)
+		local previous = (#ro.stack > 1) and ro.stack[#ro.stack - 1] or nil
+		local current = ro.stack[#ro.stack]
+		-- Unload the previous scene if needed
+		if previous and (current.method == methods.switch or current.method == methods.push or current.restored) then
+			unload_scene("#" .. previous.name)
+		-- Disable the previous scene on modal push
+		elseif current.method == methods.push_modal then
+			msg.post("#" .. previous.name, "disable")
+		-- Release input focus from the previous scene on popup
+		elseif current.method == methods.popup then
+			msg.post(scene_controller_url(ro, previous.name), "release_input_focus")
+		end
+		-- Remove previous scene from the stack if it was a switch or the current scene was restored
+		if current.method == methods.switch or current.restored then
+			table.remove(ro.stack, #ro.stack - 1)
+		end
+		-- Init and enable new scene
+		msg.post(sender, "init")
+		msg.post(sender, "enable")
+		-- Pass the input to the new scene
+		if not current.restored then
+			msg.post(scene_controller_url(ro, current.name), M.messages.scene_input,
+				{router = ro.router_url, input = current.input, state = ro.states[current.name]})
+		else
+			msg.post(scene_controller_url(ro, current.name), M.messages.scene_popped,
+				{router = ro.router_url, output = current.output, state = ro.states[current.name]})
+		end
+	end,
+
+	-- Scene is unloaded
+	[hash("proxy_unloaded")] = function()
+		-- Currently we don't need to do anything here
+	end,
+
+	-- Handle scene push
+	[messages.push] = function(ro, message)
+		next_scene(ro, methods.push, message)
+	end,
+
+	-- Handle scene modal push
+	[messages.push_modal] = function(ro, message)
+		next_scene(ro, methods.push_modal, message)
+	end,
+
+	-- Handle scene popup
+	[messages.popup] = function(ro, message)
+		next_scene(ro, methods.popup, message)
+	end,
+
+	-- Handle scene close
+	[messages.close] = function(ro, message)
+		local current = ro.stack[#ro.stack]
+		-- This scene appeared by switching, the state-machine will be used
+		if current.method == methods.switch then
+			next_scene(ro, methods.switch, message)
+		-- It was a pushed scene, we need to restore the previous one
+		elseif current.method == methods.push then
+			next_scene(ro, methods.restore, message)
+		-- It was modally pushed scene, unload it and enable the previous one
+		elseif current.method == methods.push_modal or current.method == methods.popup then
+			unload_scene("#" .. current.name)
+			table.remove(ro.stack)
+			local previous = ro.stack[#ro.stack]
+			if current.method == methods.popup then
+				msg.post(scene_controller_url(ro, previous.name), "acquire_input_focus")
+			else
+				msg.post("#" .. previous.name, "enable")
+			end
+			msg.post(scene_controller_url(ro, previous.name),
+				M.messages.scene_popped,
+				{router = ro.router_url, output = message.output, name = current.name})
+		end
+	end
+}
 
 
 ----------------------------------------------------------------------------------------------------
@@ -97,19 +176,19 @@ end
 -- @tparam string router_url Script with the router URL string
 -- @tparam string scene_controller_path Path to scene controller scripts
 function M.new(routing, router_url, scene_controller_path)
-    -- Init scene stack and scene state storage
-    local ro = {
-        stack = {},
-        states = {},
-        router_url = router_url,
-        scene_controller_path = scene_controller_path
-    }
-    -- Save router object and routing table in the internal storage.
-    router_objects[router_url] = ro
-    routing_tables[router_url] = routing
-    -- Load the first scene
-    next_scene(ro, methods.switch)
-    return router_url
+	-- Init scene stack and scene state storage
+	local ro = {
+		stack = {},
+		states = {},
+		router_url = router_url,
+		scene_controller_path = scene_controller_path
+	}
+	-- Save router object and routing table in the internal storage.
+	router_objects[router_url] = ro
+	routing_tables[router_url] = routing
+	-- Load the first scene
+	next_scene(ro, methods.switch)
+	return router_url
 end
 
 --- Router message handler
@@ -119,70 +198,10 @@ end
 -- @tparam table message Message table
 -- @param sender Message sender
 function M.on_message(router_id, message_id, message, sender)
-    local ro = router_objects[router_id]
-    -- A new scene is loaded
-    if message_id == hash("proxy_loaded") then
-        local previous = (#ro.stack > 1) and ro.stack[#ro.stack - 1] or nil
-        local current = ro.stack[#ro.stack]
-        -- Unload the previous scene if needed
-    	if previous and (current.method == methods.switch or current.method == methods.push or current.restored) then
-            unload_scene("#" .. previous.name)
-        -- Disable the previous scene on modal push
-    	elseif current.method == methods.push_modal then
-            msg.post("#" .. previous.name, "disable")
-        -- Release input focus from the previous scene on popup
-        elseif current.method == methods.popup then
-            msg.post(scene_controller_url(ro, previous.name), "release_input_focus")
-        end
-        -- Remove previous scene from the stack if it was a switch or the current scene was restored
-        if current.method == methods.switch or current.restored then
-            table.remove(ro.stack, #ro.stack - 1)
-        end
-		-- Init and enable new scene
-    	msg.post(sender, "init")
-    	msg.post(sender, "enable")
-        -- Pass the input to the new scene
-        if not current.restored then
-            msg.post(scene_controller_url(ro, current.name), M.messages.scene_input,
-                {router = router_id, input = current.input, state = ro.states[current.name]})
-        else
-            msg.post(scene_controller_url(ro, current.name), M.messages.scene_popped,
-                {router = router_id, output = current.output, state = ro.states[current.name]})
-        end
-    -- Scene is unloaded
-    elseif message_id == hash("proxy_unloaded") then
-        -- Currently we don't need to do anything here
-    -- Handle scene modal push
-    elseif message_id == messages.push_modal then
-        next_scene(ro, methods.push_modal, message)
-    -- Handle scene popup
-    elseif message_id == messages.popup then
-        next_scene(ro, methods.popup, message)
-    -- Handle scene push
-    elseif message_id == messages.push then
-        next_scene(ro, methods.push, message)
-    -- Handle scene close
-    elseif message_id == messages.close then
-        local current = ro.stack[#ro.stack]
-        -- This scene appeared by switching, the state-machine will be used
-        if current.method == methods.switch then
-            next_scene(ro, methods.switch, message)
-        -- It was a pushed scene, we need to restore the previous one
-        elseif current.method == methods.push then
-            next_scene(ro, methods.restore, message)
-        -- It was modally pushed scene, unload it and enable the previous one
-        elseif current.method == methods.push_modal or current.method == methods.popup then
-            unload_scene("#" .. current.name)
-            table.remove(ro.stack)
-            local previous = ro.stack[#ro.stack]
-            if current.method == methods.popup then
-                msg.post(scene_controller_url(ro, previous.name), "acquire_input_focus")
-            else
-                msg.post("#" .. previous.name, "enable")
-            end
-            msg.post(scene_controller_url(ro, previous.name), M.messages.scene_popped, {router = router_id, output = message.output, name = current.name})
-        end
-    end
+	local ro = router_objects[router_id]
+	if message_handlers[message_id] then
+		message_handlers[message_id](ro, message, sender)
+	end
 end
 
 --- Push the new scene to scene stack synchrounously
@@ -195,29 +214,21 @@ end
 -- @tparam table input Input for the new scene (optional)
 -- @tparam table state State of the current scene (optional)
 function M.push_sync(router_id, scene_name, input, state)
-    local ro = router_objects[router_id]
-    msg.post(ro.router_url, messages.push,
-        {scene_name = scene_name, input = input, state = state, sync = true})
+	local ro = router_objects[router_id]
+	msg.post(ro.router_url, messages.push,
+		{scene_name = scene_name, input = input, state = state, sync = true})
 end
 
 --- Push the new scene to scene stack asynchronously
--- The current scene will be unloaded and its state will be saved.
--- When the pushed scene is closed, the current scene will receive "scene_popped"
--- message with the output of the pushed scene and the state of the current scene
--- so it can restore its state. "scene_input" message will not be sent.
--- @tparam string router_id Router ID
--- @tparam string scene_name Name of the new scene
--- @tparam table input Input for the new scene (optional)
--- @tparam table state State of the current scene (optional)
 function M.push_async(router_id, scene_name, input, state)
-    local ro = router_objects[router_id]
-    msg.post(ro.router_url, messages.push,
-        {scene_name = scene_name, input = input, state = state, sync = false})
+	local ro = router_objects[router_id]
+	msg.post(ro.router_url, messages.push,
+		{scene_name = scene_name, input = input, state = state, sync = false})
 end
 
 --- Compatibility alias for push_sync
 function M.push(router_id, scene_name, input, state)
-    M.push_sync(router_id, scene_name, input, state)
+	M.push_sync(router_id, scene_name, input, state)
 end
 
 
@@ -230,28 +241,21 @@ end
 -- @tparam string scene_name Name of the new scene
 -- @tparam table input Input for the new scene (optional)
 function M.push_modal_sync(router_id, scene_name, input)
-    local ro = router_objects[router_id]
-    msg.post(ro.router_url, messages.push_modal,
-        {scene_name = scene_name, input = input, sync = true})
+	local ro = router_objects[router_id]
+	msg.post(ro.router_url, messages.push_modal,
+		{scene_name = scene_name, input = input, sync = true})
 end
 
 --- Push the new modal scene asynchrounously
--- The current scene will not be unloaded but will be disabled
--- (it will disappear from the screen). When the pushed scene is closed,
--- the current scene will receive "scene_popped" message with the output of
--- the pushed scene. State saving is not required for this method.
--- @tparam string router_id Router ID
--- @tparam string scene_name Name of the new scene
--- @tparam table input Input for the new scene (optional)
 function M.push_modal_async(router_id, scene_name, input)
-    local ro = router_objects[router_id]
-    msg.post(ro.router_url, messages.push_modal,
-        {scene_name = scene_name, input = input, sync = false})
+	local ro = router_objects[router_id]
+	msg.post(ro.router_url, messages.push_modal,
+		{scene_name = scene_name, input = input, sync = false})
 end
 
 --- Compatibility alias for push_modal_sync
 function M.push_modal(router_id, scene_name, input)
-    M.push_modal_sync(router_id, scene_name, input)
+	M.push_modal_sync(router_id, scene_name, input)
 end
 
 --- Show popup scene synchrounously
@@ -263,28 +267,21 @@ end
 -- @tparam string scene_name Name of the new scene
 -- @tparam table input Input for the new scene (optional)
 function M.popup_sync(router_id, scene_name, input)
-    local ro = router_objects[router_id]
-    msg.post(ro.router_url, messages.popup,
-        {scene_name = scene_name, input = input, sync = true})
+	local ro = router_objects[router_id]
+	msg.post(ro.router_url, messages.popup,
+		{scene_name = scene_name, input = input, sync = true})
 end
 
 --- Show popup scene asynchrounously
--- The current scene will not be unloaded but the input will be disabled.
--- When the pushed scene is closed, the current scene will receive "scene_popped"
--- message with the output of the pushed scene. The input focus will be acquired again.
--- State saving is not required for this method
--- @tparam string router_id Router ID
--- @tparam string scene_name Name of the new scene
--- @tparam table input Input for the new scene (optional)
 function M.popup_async(router_id, scene_name, input)
-    local ro = router_objects[router_id]
-    msg.post(ro.router_url, messages.popup,
-        {scene_name = scene_name, input = input, sync = false})
+	local ro = router_objects[router_id]
+	msg.post(ro.router_url, messages.popup,
+		{scene_name = scene_name, input = input, sync = false})
 end
 
 --- Compatibility alias for popup_sync
 function M.popup(router_id, scene_name, input)
-    M.popup_sync(router_id, scene_name, input)
+	M.popup_sync(router_id, scene_name, input)
 end
 
 --- Close the current scene
@@ -296,9 +293,9 @@ end
 -- @tparam table output The output of the current scene (optional)
 -- @tparam table state State of the scene (optional)
 function M.close(router_id, output, state)
-    local ro = router_objects[router_id]
-    msg.post(ro.router_url, messages.close,
-        {output = output, state = state})
+	local ro = router_objects[router_id]
+	msg.post(ro.router_url, messages.close,
+		{output = output, state = state})
 end
 
 return M
